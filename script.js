4396cde6be74b0badb0e7b29c472ac51cd33bbea
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-01-19 16:47:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
-
-alert(numberOfBooksRead());
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
+  
+  const numberOfBooksRead = () => {
+    // write your code here
+    let booksread = 0;
+    library.forEach((book) => {
+        book.readingStatus && ++booksread
+    })
+    return booksread
+  };
+  
+  // Do not change the code below
+  
+  alert(numberOfBooksRead());
+  
